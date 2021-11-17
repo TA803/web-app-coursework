4b54f7ebb266105c22e7a096569d28ccf05ae0ec
Thouseef Ali <thouseefali@outlook.com>
2021-11-17 23:38:53 +0000

    subjects

---
+++ subjects.js
@@ -0,0 +1,89 @@
+let subjects = [
+    {"id": 1001,
+    "Title":"Maths",
+    "Place":"London",
+    "Price":100,
+    "Availability":10
+    "image": ,
+},
+{
+    {"id": 1002,
+    "Title":"English",
+    "Place":"Birmingham",
+    "Price":105,
+    "Availability":9
+    "image": ,
+},
+{
+    {"id": 1003,
+    "Title":"History",
+    "Place":"Hastings",
+    "Price":250,
+    "Availability":15
+    "image": ,
+},
+{
+    {"id": 1004,
+    "Title":"Geography",
+    "Place":"Stonehenge",
+    "Price":250,
+    "Availability":12
+    "image": ,
+},
+{
+    {"id": 1005,
+    "Title":"Art",
+    "Place":"Edinburgh",
+    "Price":75,
+    "Availability":4
+    "image": ,
+},
+{
+    {"id": 1006,
+    "Title":"Football",
+    "Place":"Manchester",
+    "Price":350,
+    "Availability":5
+    "image": ,
+},
+{
+    {"id": 1007,
+    "Title":"Cricket",
+    "Place":"Lords",
+    "Price":300,
+    "Availability":7
+    "image": ,
+},
+{
+    {"id": 1008,
+    "Title":"Politics",
+    "Place":"Brighton",
+    "Price":50,
+    "Availability":15
+    "image": ,
+},
+{
+    {"id": 1009,
+    "Title":"Economics",
+    "Place":"London",
+    "Price":275,
+    "Availability":9
+    "image": ,
+},
+{
+    {"id": 1010,
+    "Title":"Business",
+    "Place":"London",
+    "Price":150,
+    "Availability":11
+    "image": ,
+},
+{
+    {"id": 1011,
+    "Title":"Nursing",
+    "Place":"Cornwall",
+    "Price":400,
+    "Availability":8
+    "image": ,
+},
+]
