9f471b61f833d1f88e2ac907af85ef1192e79460
Thouseef ALI <thouseefali@outlook.com>
2021-11-18 16:55:35 +0000

    added listing subjects and checkout button

--- subjects.js
+++ subjects.js
@@ -1,90 +1,99 @@
-let subjects = [
-    {"id": 1001,
-    "Title":"Maths",
-    "Place":"London",
-    "Price":100,
-    "Availability":10
-    "Availability":8
-    "image": https://tinyurl.com/u7439vp3 ,
-},
-{
-    {"id": 1002,
-    "Title":"English",
-    "Place":"Birmingham",
-    "Price":105,
-    "Availability":9
-    "image": ,
-},
-{
-    {"id": 1003,
-    "Title":"History",
-    "Place":"Hastings",
-    "Price":250,
-    "Availability":15
-    "image": ,
-},
-{
-    {"id": 1004,
-    "Title":"Geography",
-    "Place":"Stonehenge",
-    "Price":250,
-    "Availability":12
-    "image": ,
-},
-{
-    {"id": 1005,
-    "Title":"Art",
-    "Place":"Edinburgh",
-    "Price":75,
-    "Availability":4
-    "image": ,
-},
-{
-    {"id": 1006,
-    "Title":"Football",
-    "Place":"Manchester",
-    "Price":350,
-    "Availability":5
-    "image": ,
-},
-{
-    {"id": 1007,
-    "Title":"Cricket",
-    "Place":"Lords",
-    "Price":300,
-    "Availability":7
-    "image": ,
-},
-{
-    {"id": 1008,
-    "Title":"Politics",
-    "Place":"Brighton",
-    "Price":50,
-    "Availability":15
-    "image": ,
-},
-{
-    {"id": 1009,
-    "Title":"Economics",
-    "Place":"London",
-    "Price":275,
-    "Availability":9
-    "image": ,
-},
-{
-    {"id": 1010,
-    "Title":"Business",
-    "Place":"London",
-    "Price":150,
-    "Availability":11
-    "image": ,
-},
-{
-    {"id": 1011,
-    "Title":"Nursing",
-    "Place":"Cornwall",
-    "Price":400,
-    "Availability":8
-    "image": ,
-},
+let subjects = [{
+        "id": 1001,
+        "title": "Maths",
+        "location": "London",
+        "price": 100,
+        "space": 5,
+        "image": "https://tinyurl.com/u7439vp3",
+    },
+    {
+
+        "id": 1002,
+        "title": "English",
+        "location": "Birmingham",
+        "price": 105,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1003,
+        "title": "History",
+        "location": "Hastings",
+        "price": 250,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1004,
+        "title": "Geography",
+        "location": "Stonehenge",
+        "price": 250,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1005,
+        "title": "Art",
+        "location": "Edinburgh",
+        "price": 75,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1006,
+        "title": "Football",
+        "location": "Manchester",
+        "price": 350,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1007,
+        "title": "Cricket",
+        "location": "Lords",
+        "price": 300,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1008,
+        "title": "Politics",
+        "location": "Brighton",
+        "price": 50,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1009,
+        "title": "Economics",
+        "location": "London",
+        "price": 275,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1010,
+        "title": "Business",
+        "location": "London",
+        "price": 150,
+        "space": 5,
+        "image": 'math.png',
+    },
+    {
+
+        "id": 1011,
+        "title": "Nursing",
+        "location": "Cornwall",
+        "price": 400,
+        "space": 5,
+        "image": 'math.png',
+    },
 ]
